a5aef65448f5ec16d25c5ea91bcd1e0f10958d0b
Hasibul Chowdhury <hasibulc91@gmail.com>
2019-10-25 22:38:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(string)
   string_array = string.split(' ')
-  string_array.collect{|n| n.reverse}
-  string_reversed = string_array.join(' ')
+  reversed_array = string_array.collect{|n| n.reverse}
+  string_reversed = reversed_array.join(' ')
 end
